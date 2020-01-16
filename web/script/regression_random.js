70961d532c233f101b03c433690938da8b843ba3
Santha Lakshmi Narayana <santhalakshminarayana@gmail.com>
2020-01-16 17:46:01 +0530

    completed gui for regression

--- web/script/regression_random.js
+++ web/script/regression_random.js
@@ -1,26 +1,36 @@
 $(document).ready(function(){
 
-	function get_parameters(name)
+	function get_parameters_text(id, default_val)
+	{
+		var para = $('#' + id).val();
+		if($.isEmptyObject(para))
+			return default_val;
+		else
+			return para;
+	}
+
+	function get_parameters_box(name, default_val)
 	{
 		var para = [];
 		$.each($("[name = " + name + "]"), function(){
 			if($(this).css('background-color') == 'rgb(91, 192, 222)') 
-			{
 				para.push($(this).html());
-			}
 		});
-		return para.join(',');
+		if($.isEmptyObject(para))
+			return default_val;
+		else
+			return para.join(',');
 	}
 
 	$('#next_button').on('click',function(){
 
-		var n_estimators = $('#n_estimators').val();
-		var criterion = get_parameters('criterion');
-		var max_depth = $('#max_depth').val();
-		var min_samples_split = $('#min_samples_split').val();
-		var max_features = get_parameters('max_features');
-		var max_leaf_nodes = $('#max_leaf_nodes').val();
-		var bootstrap = get_parameters('bootstrap');
+		var n_estimators = get_parameters_text('n_estimators', '10')
+		var criterion = get_parameters_box('criterion', 'mse');
+		var max_depth = get_parameters_text('max_depth', 'None')
+		var min_samples_split = get_parameters_text('min_samples_split', '2')
+		var max_features = get_parameters_box('max_features', 'auto');
+		var max_leaf_nodes = get_parameters_text('max_leaf_nodes', 'None');
+		var bootstrap = get_parameters_box('bootstrap', 'True');
 
 		alert(n_estimators);
 		alert(criterion);
--- web/script/svr.js
+++ web/script/svr.js
@@ -1,17 +1,32 @@
 $(document).ready(function(){
-	$('#next_button').on('click',function(){
-		var kernel = [];
-		$.each($("[name = 'kernel']"), function(){
+
+	function get_parameters_text(id, default_val)
+	{
+		var para = $('#' + id).val();
+		if($.isEmptyObject(para))
+			return default_val;
+		else
+			return para;
+	}
+
+	function get_parameters_box(name, default_val)
+	{
+		var para = [];
+		$.each($("[name = " + name + "]"), function(){
 			if($(this).css('background-color') == 'rgb(91, 192, 222)') 
-			{
-				kernel.push($(this).html());
-			}
+				para.push($(this).html());
 		});
-		kernel = kernel.join(',');
-		
-		var gamma = $('#gamma').val();
-		var C = $('#C').val();
-		var epsilon = $('#epsilon').val();
+		if($.isEmptyObject(para))
+			return default_val;
+		else
+			return para.join(',');
+	}
+
+	$('#next_button').on('click',function(){
+		var kernel = get_parameters_box('kernel', 'rbf');
+		var gamma = get_parameters_text('gamma', 'scale');
+		var C = get_parameters_text('C', '1.0')
+		var epsilon = get_parameters_text('epsilon', '0.1');
 		
 		alert(kernel);
 		alert(gamma);
